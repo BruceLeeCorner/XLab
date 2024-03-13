9fc8edab6fd8f6be923ee336af55dfe011818d12
Bruce Lee <67289897+euvio@users.noreply.github.com>
2024-03-13 19:05:06 +0800

    .

---
+++ XLab/CaliburnLab/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace CaliburnLab
+{
+    public class MainViewModel:PropertyChangedBase
+    {
+
+        private decimal _salary;
+        private string _phone;
+        private string _mail;
+
+        public decimal Salary
+        {
+            get => _salary;
+            // 缺省时，CallerMemberName决定通知Salary
+            set => Set(ref _salary, value);
+        }
+
+        public string Mail
+        {
+            get => _mail;
+            // 显示指定通知Mail
+            set => Set(ref _mail, value, nameof(Mail));
+        }
+
+        public string Phone
+        {
+            get => _phone;
+            // 显示指定通知MainViewModel的所有属性
+            set => Set(ref _phone, value, string.Empty);
+        }
+    }
+}
